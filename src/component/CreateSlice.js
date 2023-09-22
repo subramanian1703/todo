bf04764404b3882e1d872078e19f7d37af91b325
Subramanian vasan <132151840+subramanian1703@users.noreply.github.com>
2023-09-22 15:13:37 +0530

    Update CreateSlice.js

--- src/component/CreateSlice.js
+++ src/component/CreateSlice.js
@@ -1,1 +1,26 @@
-c
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const developerSlice = createSlice({
+  name: "developer",
+  initialState: {
+    developers: [],
+  },
+  reducers: {
+    AddDeveloper: (state, action) => {
+      state.developers = [...state.developers, action.payload];
+    },
+    EditDeveloper: (state, action) => {
+      const { selectedIndex, developer } = action.payload;
+      state.developers[selectedIndex] = developer;
+    },
+    RemoveDeveloper: (state, { payload }) => {
+      state.developers.splice(payload, 1);
+    },
+  },
+});
+
+export const { AddDeveloper, EditDeveloper, RemoveDeveloper } =
+  developerSlice.actions;
+
+export default developerSlice.reducer;
